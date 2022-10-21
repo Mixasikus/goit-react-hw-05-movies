d2acbc130d3b16b56d928930ce3479632608f79c
Mixasikus <mixasiikus@gmail.com>
2022-10-21 20:03:54 +0300

    .

--- src/Services/GetMovieCredits.jsx
+++ src/Services/GetMovieCredits.jsx
@@ -5,7 +5,7 @@ import 'react-toastify/dist/ReactToastify.css';
 export default function GetMovieCredits(movieId) {
   return axios
     .get(
-      `https://ai.themoviedb.org/3/movie/${movieId}/credits?api_key=ff98b74c6ada2972698b8eff6707845a&language=en-US&page=1`
+      `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=ff98b74c6ada2972698b8eff6707845a&language=en-US&page=1`
     )
     .catch(r => {
       if (r.data === undefined) {
